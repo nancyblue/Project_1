0ccfaea11235ce4fb0768b3b003f896122cd7666
barbi105 <bontiverosg@gmail.com>
2019-03-20 14:29:33 -0500

    trails appear on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
 
     function renderTrails(latitude, longitude) {
         //change limit to 30 
-        var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=05&maxResults=30&key=200432797-9adce9a5420c2e2c01a8fe63186f4f81";
+        var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=30&maxResults=30&key=200432797-9adce9a5420c2e2c01a8fe63186f4f81";
 
         $.ajax({
             url: queryURL,
@@ -84,20 +84,20 @@ $(document).ready(function () {
                 //take the information from the aray (30)
                 //for each item make a div
                 var trailDiv = $("<div class='trailDiv'></div>");
-                //img small
-                var image = $("<img>").attr("src", results[i].imgSmall);
                 //trail name
                 var p1 = $("<p>").text("Trail Name: " + results[i].name);
                 //location
                 var p2 = $("<p>").text("Location: " + results[i].location);
-                //length
-                var p6 = $("<p>").text("Length (Miles): " + results[i].length);
                 //difficulty 
                 var p3 = $("<p>").text("Difficulty: " + results[i].difficulty);
                 //condition details 
                 var p4 = $("<p>").text("Trail Condition: " + results[i].conditionDetails);
                 //ascent 
                 var p5 = $("<p>").text("Ascent (Feet): " + results[i].ascent);
+                //length
+                var p6 = $("<p>").text("Length (Miles): " + results[i].length);
+                //img small
+                var image = $("<img>").attr("src", results[i].imgSmall);
 
                 trailDiv.append(image, p1, p2, p6, p3, p4, p5);
 
